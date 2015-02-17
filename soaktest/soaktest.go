b0d7b40d87898cd9334d78e4b6f714f6940aed7b
Bryan Boreham <bjboreham@gmail.com>
2015-02-17 14:25:43 +0000

    Set up veth; refactor

--- soaktest/soaktest.go
+++ soaktest/soaktest.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"github.com/docker/libcontainer/netlink"
+	"github.com/docker/libcontainer/system"
 	"github.com/fsouza/go-dockerclient"
 	lg "github.com/zettio/weave/common"
 	"net"
+	"os"
+	"runtime"
 	"strings"
+	"time"
 )
 
 type weaver struct {
@@ -14,65 +20,132 @@ type weaver struct {
 }
 
 func main() {
-	makeWeaves(1)
+	makeWeaves(3)
 }
 
-func makeWeaves(n int) {
-	namePrefix := "testweave"
-	apiPath := "unix:/var/run/docker.sock"
-	client, err := docker.NewClient(apiPath)
-	checkError(err, apiPath)
+type testContext struct {
+	apiPath string
+	dc      *docker.Client
+}
 
-	env, err := client.Version()
-	checkError(err, apiPath)
+const namePrefix = "testweave"
+
+func makeWeaves(n int) {
+	context := &testContext{apiPath: "unix:/var/run/docker.sock"}
+	var err error
+	context.dc, err = docker.NewClient(context.apiPath)
+	check(err, context.apiPath)
+
+	env, err := context.dc.Version()
+	check(err, context.apiPath)
 
 	events := make(chan *docker.APIEvents)
-	err = client.AddEventListener(events)
-	checkError(err, apiPath)
+	err = context.dc.AddEventListener(events)
+	check(err, context.apiPath)
 
-	lg.Info.Printf("[updater] Using Docker API on %s: %v", apiPath, env)
+	lg.Info.Printf("[updater] Using Docker API on %s: %v", context.apiPath, env)
 
-	containers, _ := client.ListContainers(docker.ListContainersOptions{All: true})
+	context.deleteOldContainers()
+
+	// Give Docker and/or Linux time to react - if we don't sleep here
+	// we get "Network interface already exists" from NetworkCreateVethPair
+	time.Sleep(200 * time.Millisecond)
+
+	// Start the Weave containers
+	for i := 0; i < n; i++ {
+		name := fmt.Sprintf("%s%d", namePrefix, i)
+		context.startOneWeave(name)
+	}
+}
+
+// Delete any old containers created by this test prog
+func (context *testContext) deleteOldContainers() {
+	containers, _ := context.dc.ListContainers(docker.ListContainersOptions{All: true})
 	for _, cont := range containers {
 		for _, name := range cont.Names {
 			if strings.HasPrefix(name, "/"+namePrefix) {
 				if strings.HasPrefix(cont.Status, "Up") {
 					lg.Info.Println("Killing", name)
-					checkFatal(client.KillContainer(docker.KillContainerOptions{ID: cont.ID}))
+					checkFatal(context.dc.KillContainer(docker.KillContainerOptions{ID: cont.ID}))
 				}
 				lg.Info.Println("Removing", cont.ID, cont.Names, cont.Status)
-				checkFatal(client.RemoveContainer(docker.RemoveContainerOptions{ID: cont.ID}))
+				checkFatal(context.dc.RemoveContainer(docker.RemoveContainerOptions{ID: cont.ID}))
 				break
-			} else {
-				lg.Info.Println("Ignoring", cont.ID, cont.Names, cont.Status)
 			}
 		}
 	}
+}
+
+func (context *testContext) startOneWeave(name string) *docker.Container {
+	iface1, iface2 := createVeths(name)
 
 	config := &docker.Config{
 		Image: "zettio/weave",
+		Cmd:   []string{"-iface", "ethwe", "-api", "none"},
 	}
-	for i := 0; i < n; i++ {
-		name := fmt.Sprintf("%s%d", namePrefix, i)
-		opts := docker.CreateContainerOptions{Name: name, Config: config}
-		lg.Info.Println("Creating", name)
-		cont, err := client.CreateContainer(opts)
-		checkFatal(err)
-		cont.Args = []string{"-iface", "lo0"}
-		err = client.StartContainer(cont.ID, nil)
-		checkFatal(err)
+	opts := docker.CreateContainerOptions{Name: name, Config: config}
+	lg.Info.Println("Creating", name)
+	cont, err := context.dc.CreateContainer(opts)
+	checkFatal(err)
+
+	checkFatal(context.dc.StartContainer(cont.ID, nil))
+	cont, err = context.dc.InspectContainer(cont.ID)
+	checkFatal(err)
+	if !cont.State.Running {
+		lg.Error.Fatalf("Container %s (%s) exited immediately", name, cont.ID)
 	}
+	setupNetwork(cont, iface1, iface2)
+	return cont
 }
 
-func checkError(err error, apiPath string) {
-	if err != nil {
-		lg.Error.Fatalf("Unable to connect to Docker API on %s: %s",
-			apiPath, err)
+func createVeths(name string) (*net.Interface, *net.Interface) {
+	vethname1, vethname2 := name+"x", name+"y"
+	check(netlink.NetworkCreateVethPair(vethname1, vethname2, 42), "Creating veth pair")
+	iface1, err := net.InterfaceByName(vethname1)
+	check(err, "Finding interface")
+	iface2, err := net.InterfaceByName(vethname2)
+	check(err, "Finding interface")
+	return iface1, iface2
+}
+
+func setupNetwork(cont *docker.Container, iface1, iface2 *net.Interface) {
+	check(netlink.NetworkSetNsPid(iface2, cont.State.Pid), "Setting namespace")
+	check(withNetnsPid(cont.State.Pid, func() error {
+		if err := netlink.NetworkChangeName(iface2, "ethwe"); err != nil {
+			return err
+		}
+		if err := netlink.NetworkLinkUp(iface2); err != nil {
+			return err
+		}
+		return nil
+	}), "with netns")
+	check(netlink.NetworkLinkUp(iface1), "link up")
+}
+
+// equivalent of Weave script with_container_netns()
+func withNetnsPid(pid int, f func() error) error {
+	name := fmt.Sprintf("/proc/%d/ns/net", pid)
+	if pidnsfile, err := os.Open(name); err != nil {
+		return errors.New("Unable to open " + name + ": " + err.Error())
+	} else if selfnsfile, err := os.Open("/proc/self/ns/net"); err != nil {
+		return errors.New("Unable to open /proc/self/ns/net: " + err.Error())
+	} else {
+		runtime.LockOSThread()
+		defer runtime.UnlockOSThread()
+		system.Setns(pidnsfile.Fd(), 0)
+		defer system.Setns(selfnsfile.Fd(), 0)
+		return f()
 	}
 }
 
 func checkFatal(err error) {
 	if err != nil {
 		lg.Error.Fatalf("Fatal error: %s", err)
+	}
+}
+
+func check(err error, desc string) {
+	if err != nil {
+		lg.Error.Fatalf("%s: %s", desc, err)
 	}
 }
